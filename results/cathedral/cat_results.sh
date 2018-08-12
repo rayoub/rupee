9b2533abd2227a307cb0762cfe69e8fd26f56c53
Ronald Ayoub <ronaldayoub@outlook.com>
2018-08-12 07:01:03 -0500

    rename

--- results/cathedral/cat_results.sh
+++ results/cathedral/cat_results.sh
@@ -2,7 +2,7 @@
 
 dir=$1
 
-for id in $(find ${dir} -name '*.results' -printf '%f\n'); do
-    awk -v id=${id%.results} -e "BEGIN { OFS = \",\" } { print id, \$0 }" ${dir}/${id}
+for id in $(find ${dir} -name '*.txt' -printf '%f\n'); do
+    awk -v id=${id%.txt} -e "BEGIN { OFS = \",\" } { print id, \$0 }" ${dir}/${id}
 done;
 
--- results/rupee/cat_results.sh
+++ results/rupee/cat_results.sh
@@ -2,7 +2,7 @@
 
 dir=$1
 
-for id in $(find ${dir} -name '*.results' -printf '%f\n'); do
+for id in $(find ${dir} -name '*.txt' -printf '%f\n'); do
     cat ${dir}/${id}
 done;
 
