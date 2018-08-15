983d072a55c253c6ec6ab7ed68fbef6f15576d20
Ronald Ayoub <ronaldayoub@outlook.com>
2018-08-15 10:32:46 -0500

    increment

--- results/rupee/rupee.sh
+++ results/rupee/rupee.sh
@@ -6,4 +6,4 @@ bm=$1
 ver=$2
 id=$3
 
-java -jar ../../rupee-mgr/target/rupee-mgr-0.0.1-SNAPSHOT-jar-with-dependencies.jar -s DB_ID,CATH,CATH,${id},400,FALSE,FALSE,TRUE,FALSE,FALSE,FALSE,CE,TM_SCORE > ./${bm}-${ver}/${id}.txt
+java -jar ../../rupee-mgr/target/rupee-mgr-0.0.1-SNAPSHOT-jar-with-dependencies.jar -s DB_ID,SCOP,SCOP,${id},400,FALSE,FALSE,TRUE,FALSE,FALSE,FALSE,CE,TM_SCORE > ./${bm}_${ver}/${id}.txt
