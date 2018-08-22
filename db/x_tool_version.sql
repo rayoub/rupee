fdf6a96686b9fc35aa99cae3235af89b4b6965f4
Ronald Ayoub <ronaldayoub@outlook.com>
2018-08-22 11:06:11 -0500

    results

--- db/x_tool_version.sql
+++ db/x_tool_version.sql
@@ -4,9 +4,7 @@ TRUNCATE tool_version;
 INSERT INTO tool_version (tool, version)
 VALUES 
     ('rupee',       'cath_v4_1_0'),
-    ('rupee',       'cath_v4_2_0'),
     ('rupee',       'scop_v1_73'),
-    ('rupee',       'scop_v2_06'),
     ('rupee',       'scop_v2_07'),
     ('cathedral',   'cath_v4_1_0'),
     ('mtm',         'dom_v08_03_2018'),
