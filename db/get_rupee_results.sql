0f3b9856cb3fad8758673c548cc5b4d5273d06d9
Ronald Ayoub <ronaldayoub@outlook.com>
2018-08-22 13:06:25 -0500

    eval

--- db/get_rupee_results.sql
+++ db/get_rupee_results.sql
@@ -1,14 +1,11 @@
 
-CREATE OR REPLACE FUNCTION get_rupee_results (p_limit INTEGER)
+CREATE OR REPLACE FUNCTION get_rupee_results (p_benchmark VARCHAR, p_version VARCHAR, p_limit INTEGER)
 RETURNS TABLE (
-    n BIGINT, 
-    cath_id_1 VARCHAR,
-    cath_id_2 VARCHAR,
-    rupee_score NUMERIC,
-    cecp_rmsd NUMERIC,
-    cecp_tm_score NUMERIC,
-    fatcat_rmsd NUMERIC,
-    fatcat_tm_score NUMERIC
+    n INTEGER, 
+    db_id_1 VARCHAR,
+    db_id_2 VARCHAR,
+    ce_rmsd NUMERIC,
+    ce_tm_score NUMERIC
 )
 AS $$
 BEGIN
@@ -17,58 +14,56 @@ BEGIN
     WITH results AS
     (
         SELECT
-            ROW_NUMBER() OVER (PARTITION BY r.cath_id_1 ORDER BY r.rupee_score DESC, r.cath, r.solid) AS n,
-            COUNT(*) OVER (PARTITION BY r.cath_id_1) AS tot,
-            r.cath_id_1,
-            r.cath_id_2,
-            r.rupee_score,
-            r.cecp_rmsd,
-            r.cecp_tm_score,
-            r.fatcat_rmsd,
-            r.fatcat_tm_score
+            COUNT(*) OVER (PARTITION BY r.db_id_1) AS tot,
+            r.n,
+            r.db_id_1,
+            r.db_id_2,
+            r.ce_rmsd,
+            r.ce_tm_score
         FROM
             rupee_result r
+            INNER JOIN benchmark b
+                ON b.db_id = r.db_id_1
+                AND b.name = p_benchmark
+        WHERE
+            r.version = p_version 
     ),
     valid_results As
     (
+        -- first result matches query 
+        -- at least limit number of results
         SELECT
-            r.cath_id_1 AS cath_id
+            r.db_id_1 AS db_id
         FROM
             results r
         WHERE
-            r.n = 1 AND r.cath_id_1 = r.cath_id_2 AND r.tot >= p_limit
+            r.n = 1 AND r.db_id_1 = r.db_id_2 AND r.tot >= p_limit
     ),
     filtered_results AS
     (
         SELECT
             r.n, 
-            r.cath_id_1,
-            r.cath_id_2,
-            r.rupee_score,
-            r.cecp_rmsd,
-            r.cecp_tm_score,
-            r.fatcat_rmsd,
-            r.fatcat_tm_score
+            r.db_id_1,
+            r.db_id_2,
+            r.ce_rmsd,
+            r.ce_tm_score
         FROM 
             results r
             INNER JOIN valid_results v
-                ON v.cath_id = r.cath_id_1 
+                ON v.db_id = r.db_id_1
         WHERE
             r.n <= p_limit
     )
     SELECT
         r.n,
-        r.cath_id_1,
-        r.cath_id_2,
-        r.rupee_score,
-        r.cecp_rmsd,
-        r.cecp_tm_score,
-        r.fatcat_rmsd,
-        r.fatcat_tm_score
+        r.db_id_1,
+        r.db_id_2,
+        r.ce_rmsd,
+        r.ce_tm_score
     FROM 
         filtered_results r
     ORDER BY
-        r.cath_id_1,
+        r.db_id_1,
         r.n;
 
 END;
--- db/insert_rupee_eval.sql
+++ db/insert_rupee_eval.sql
@@ -1,5 +1,5 @@
 
-CREATE OR REPLACE FUNCTION set_rupee_eval (p_description VARCHAR, p_limit INTEGER) RETURNS VOID
+CREATE OR REPLACE FUNCTION insert_rupee_eval (p_benchmark VARCHAR, p_version VARCHAR, p_description VARCHAR, p_limit INTEGER) RETURNS VOID
 AS $$
 BEGIN
 
@@ -7,32 +7,28 @@ BEGIN
     (
         SELECT
             n,
-            cath_id_1,
-            cath_id_2,
-            cecp_rmsd,
-            cecp_tm_score,
-            fatcat_rmsd,
-            fatcat_tm_score
+            db_id_1,
+            db_id_2,
+            ce_rmsd,
+            ce_tm_score
         FROM
-            get_rupee_results(p_limit)
+            get_rupee_results(p_benchmark, p_version, p_limit)
     ),
     eval AS
     (
         SELECT
-            AVG(cecp_rmsd) AS avg_cecp_rmsd,
-            AVG(cecp_tm_score) AS avg_cecp_tm_score,
-            AVG(fatcat_rmsd) AS avg_fatcat_rmsd,
-            AVG(fatcat_tm_score) AS avg_fatcat_tm_score
+            AVG(ce_rmsd) AS avg_ce_rmsd,
+            AVG(ce_tm_score) AS avg_ce_tm_score
         FROM
             results
     )
-    INSERT INTO rupee_eval(description, avg_cecp_rmsd, avg_cecp_tm_score, avg_fatcat_rmsd, avg_fatcat_tm_score, num_rows)
+    INSERT INTO rupee_eval(benchmark, version, description, avg_ce_rmsd, avg_ce_tm_score, num_rows)
     SELECT
+        p_benchmark,
+        p_version,
         p_description,
-        e.avg_cecp_rmsd,
-        e.avg_cecp_tm_score,
-        e.avg_fatcat_rmsd,
-        e.avg_fatcat_tm_score,
+        e.avg_ce_rmsd,
+        e.avg_ce_tm_score,
         p_limit
     FROM
         eval e;
