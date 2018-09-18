ff209013fee1f74030c55b7798db20313d7f1c5e
Ronald Ayoub <ronaldayoub@outlook.com>
2018-09-18 11:44:23 -0500

    get top 100 save some space

--- results/rupee/cat_results.sh
+++ results/rupee/cat_results.sh
@@ -5,6 +5,6 @@ ver=$2
 dir=${bm}_${ver}
 
 for id in $(find ${dir} -name '*.txt' -printf '%f\n'); do
-    xargs -a ${dir}/${id} -L1 printf "%s,%s\n" ${ver}
+    head -n 100 ${dir}/${id} | xargs -L1 printf "%s,%s\n" ${ver}
 done;
 
