205d437c53d0e5ad84a7df5327708aefcc6eaa6a
Ronald Ayoub <ronaldayoub@outlook.com>
2018-10-13 04:40:57 -0500

    mode

--- results/rupee/rupee.sh
+++ results/rupee/rupee.sh
@@ -6,4 +6,4 @@ bm=$1
 ver=$2
 id=$3
 
-java -jar ../../rupee-mgr/target/rupee-mgr-0.0.1-SNAPSHOT-jar-with-dependencies.jar -s DB_ID,SCOP,SCOP,${id},2000,FALSE,FALSE,FALSE,FALSE,FALSE,FALSE,TRUE,TM_SCORE > ./${bm}_${ver}/${id}.txt
+java -jar ../../rupee-mgr/target/rupee-mgr-0.0.1-SNAPSHOT-jar-with-dependencies.jar -s DB_ID,SCOP,SCOP,${id},2000,FALSE,FALSE,FALSE,FALSE,FALSE,FALSE,REGULAR,TM_SCORE > ./${bm}_${ver}/${id}.txt
