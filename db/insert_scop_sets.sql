efbb43876657b0241e5f547ad500a6b8aa37920c
Ronald Ayoub <ronaldayoub@outlook.com>
2018-10-16 12:40:56 -0500

    ndd changes

--- db/insert_scop_sets.sql
+++ db/insert_scop_sets.sql
@@ -3,7 +3,7 @@ RETURNS VOID
 AS $$
 BEGIN
 
-    DELETE FROM scop_set WHERE similarity = -1.0;
+    TRUNCATE TABLE scop_set;
 
     INSERT INTO scop_set (pivot_db_id, member_db_id, similarity)
     SELECT
@@ -13,17 +13,17 @@ BEGIN
     FROM
         UNNEST(p_tab);
 
-    --UPDATE scop_hashes
-    --SET set_id = NULL;
+    UPDATE scop_hashes
+    SET set_id = NULL;
 
-    --UPDATE scop_hashes
-    --SET set_id = scop_set.pivot_db_id
-    --FROM scop_set
-    --WHERE db_id = scop_set.member_db_id;
+    UPDATE scop_hashes
+    SET set_id = scop_set.pivot_db_id
+    FROM scop_set
+    WHERE db_id = scop_set.member_db_id;
 
-    --UPDATE scop_hashes
-    --SET set_id = db_id
-    --WHERE set_id IS NULL;
+    UPDATE scop_hashes
+    SET set_id = db_id
+    WHERE set_id IS NULL;
                 
 END;
 $$ LANGUAGE plpgsql;
