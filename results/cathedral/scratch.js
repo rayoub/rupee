9d156f9d8f583b53ba7a192dbb6428321363f1f2
Ronald Ayoub <ronaldayoub@outlook.com>
2018-11-02 18:35:40 -0500

    remove file

--- results/cathedral/scratch.js
+++
@@ -1,2 +0,0 @@
-$('.cathedral-result').each(function() { console.log($(this).attr('data-match-id') + ',' + $(this).attr('data-cathedral-ssap-score') + ',' + $(this).attr('data-cathedral-rmsd')); });
-
