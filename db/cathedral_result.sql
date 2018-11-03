1e71a648fbe0602be11a79348f9868184fdc12ba
Ronald Ayoub <ronaldayoub@outlook.com>
2018-11-03 05:15:03 -0500

    cathedral results

--- db/cathedral_result.sql
+++ db/cathedral_result.sql
@@ -4,9 +4,7 @@ CREATE TABLE cathedral_result
     version VARCHAR NOT NULL,
     n INTEGER NOT NULL,
     db_id_1 VARCHAR NOT NULL,
-    db_id_2 VARCHAR NOT NULL,
-    ssap_score NUMERIC NOT NULL,
-    cathedral_rmsd NUMERIC NOT NULL
+    db_id_2 VARCHAR NOT NULL
 ); 
 
 CREATE UNIQUE INDEX idx_cathedral_result_unique ON cathedral_result (version, n, db_id_1);
