c3a3635234762addb2585506caf2018cf285f4d1
Ronald Ayoub <ronaldayoub@outlook.com>
2019-04-02 06:30:44 -0500

    remove ndd

--- db/scop_set.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE scop_set
-(
-    pivot_db_id VARCHAR NOT NULL,
-    member_db_id VARCHAR NOT NULL,
-    similarity NUMERIC NOT NULL
-);
-
-CREATE UNIQUE INDEX idx_scop_set_unique ON scop_set (pivot_db_id, member_db_id);
