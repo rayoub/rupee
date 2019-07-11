f794e7fd0a1fd44f3f7d77e4f4a456e7d9649890
Ronald Ayoub <ronaldayoub@outlook.com>
2019-07-11 13:00:55 -0500

    fewer threads

--- rupee-mgr/src/main/java/edu/umkc/rupee/lib/Constants.java
+++ rupee-mgr/src/main/java/edu/umkc/rupee/lib/Constants.java
@@ -48,7 +48,7 @@ public class Constants {
     public final static int DEC_POW_5 = DEC_POW_1 * DEC_POW_4;
     public final static int DEC_POW_6 = DEC_POW_1 * DEC_POW_5;
 
-    public final static int SPLIT_COUNT = 8;
+    public final static int SPLIT_COUNT = 4;
     public final static int PROCESSED_INCREMENT = 500;
 
     public final static int FETCH_SIZE = 1000;
