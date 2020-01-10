3b567008d20510e6e39a6919ba6f73387ad3a017
Ronald Ayoub <ronaldayoub@outlook.com>
2020-01-09 19:30:00 -0600

    gather data

--- db/x_benchmark.sql
+++ db/x_benchmark.sql
@@ -16,17 +16,17 @@ COPY benchmark (name, db_id) FROM '/home/ayoub/git/rupee/results/benchmarks/scop
 COPY benchmark (name, db_id) FROM '/home/ayoub/git/rupee/results/benchmarks/cath_d100.txt' WITH (DELIMITER ',');
 COPY benchmark (name, db_id) FROM '/home/ayoub/git/rupee/results/benchmarks/cath_d99.txt' WITH (DELIMITER ',');
 
-COPY benchmark (name, db_id) FROM '/home/ayoub/git/rupee/results/benchmarks/casp_d150.txt' WITH (DELIMITER ',');
+COPY benchmark (name, db_id) FROM '/home/ayoub/git/rupee/results/benchmarks/casp_d230.txt' WITH (DELIMITER ',');
 
 -- mtm specific
-COPY benchmark (name, db_id) FROM '/home/ayoub/git/rupee/results/benchmarks/casp_mtm_d144.txt' WITH (DELIMITER ',');
-COPY benchmark (name, db_id) FROM '/home/ayoub/git/rupee/results/benchmarks/casp_mtm_d34.txt' WITH (DELIMITER ',');
+--COPY benchmark (name, db_id) FROM '/home/ayoub/git/rupee/results/benchmarks/casp_mtm_d144.txt' WITH (DELIMITER ',');
+--COPY benchmark (name, db_id) FROM '/home/ayoub/git/rupee/results/benchmarks/casp_mtm_d34.txt' WITH (DELIMITER ',');
 
 -- cathedral specific
-COPY benchmark (name, db_id) FROM '/home/ayoub/git/rupee/results/benchmarks/casp_cathedral_d149.txt' WITH (DELIMITER ',');
+--COPY benchmark (name, db_id) FROM '/home/ayoub/git/rupee/results/benchmarks/casp_cathedral_d149.txt' WITH (DELIMITER ',');
 
 -- ssm specific
-COPY benchmark (name, db_id) FROM '/home/ayoub/git/rupee/results/benchmarks/casp_ssm_d149.txt' WITH (DELIMITER ',');
+--COPY benchmark (name, db_id) FROM '/home/ayoub/git/rupee/results/benchmarks/casp_ssm_d149.txt' WITH (DELIMITER ',');
 
 
 
