7dd27ac6e25507fdc8f18d0703468be2fbe96d81
Ronald Ayoub <ronaldayoub@outlook.com>
2020-02-09 14:35:24 -0600

    hhr

--- data/casp/seqs.sh
+++ data/casp/seqs.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# run like this 'xargs -a seqs.txt -L1 ./seqs.sh'
+# run like this 'xargs -a seqs_*.txt -L1 ./seqs.sh'
 
 seq=$1
 hhblits -cpu 8 -i ./seqs/${seq}.seq -d ~/git/hh-suite/databases/UniRef30_2020_01/UniRef30_2020_01 -o ./seqs/${seq}.hhr -n 3 -cov 60
