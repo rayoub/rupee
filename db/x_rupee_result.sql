510f303888d7520feb11df023a2a1a89e2ab10b9
Ronald Ayoub <ronaldayoub@outlook.com>
2020-03-31 20:16:06 -0500

    ssap

--- db/x_rupee_result.sql
+++ db/x_rupee_result.sql
@@ -14,10 +14,14 @@ COPY rupee_result (version, n, db_id_1, db_id_2, rupee_rmsd, rupee_tm_score, sea
 COPY rupee_result (version, n, db_id_1, db_id_2, rupee_rmsd, rupee_tm_score, search_mode, search_type) FROM '/home/ayoub/git/rupee/results/rupee/casp_d250_casp_scop_v1_73_top_fl.txt' WITH (DELIMITER ',');
 COPY rupee_result (version, n, db_id_1, db_id_2, rupee_rmsd, rupee_tm_score, search_mode, search_type) FROM '/home/ayoub/git/rupee/results/rupee/casp_d250_casp_cath_v4_2_0_all_fl.txt' WITH (DELIMITER ',');
 COPY rupee_result (version, n, db_id_1, db_id_2, rupee_rmsd, rupee_tm_score, search_mode, search_type) FROM '/home/ayoub/git/rupee/results/rupee/casp_d250_casp_cath_v4_2_0_top_fl.txt' WITH (DELIMITER ',');
+
+-- cathedral with casp_d250 
 COPY rupee_result (version, n, db_id_1, db_id_2, rupee_rmsd, rupee_tm_score, search_mode, search_type) FROM '/home/ayoub/git/rupee/results/rupee/casp_d250_casp_cath_v4_2_0_top_rmsd.txt' WITH (DELIMITER ',');
 
--- casp_d250 (with additional scores)
+-- cathedral with casp_d250 (additional fileds)
+COPY rupee_result (version, n, db_id_1, db_id_2, rupee_rmsd, rupee_tm_score, rupee_q_score, rupee_ssap_score, search_mode, search_type) FROM '/home/ayoub/git/rupee/results/rupee/casp_d250_casp_cath_v4_2_0_all_rmsd.txt' WITH (DELIMITER ',');
 COPY rupee_result (version, n, db_id_1, db_id_2, rupee_rmsd, rupee_tm_score, rupee_q_score, rupee_ssap_score, search_mode, search_type) FROM '/home/ayoub/git/rupee/results/rupee/casp_d250_casp_cath_v4_2_0_top_ssap.txt' WITH (DELIMITER ',');
+COPY rupee_result (version, n, db_id_1, db_id_2, rupee_rmsd, rupee_tm_score, rupee_q_score, rupee_ssap_score, search_mode, search_type) FROM '/home/ayoub/git/rupee/results/rupee/casp_d250_casp_cath_v4_2_0_all_ssap.txt' WITH (DELIMITER ',');
 
 
 
