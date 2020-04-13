3c281d27d02f90353da32f9e7c7720652c3fd064
Ronald Ayoub <ronaldayoub@outlook.com>
2020-04-13 04:42:19 -0500

    ssap

--- db/z_ssap_todo.sql
+++ db/z_ssap_todo.sql
@@ -8,7 +8,6 @@ FROM
         ON s.db_id_1 = r.db_id_1 AND s.db_id_2 = r.db_id_2
 WHERE
     r.version = 'casp_cath_v4_2_0'
-    AND r.search_mode = 'top_aligned'
     AND r.search_type = 'ssap_score'
     AND s.ssap_score = -1;
      
