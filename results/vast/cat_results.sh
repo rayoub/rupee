54bf93eacad069795d265c5cc31aeda63662f96f
Ronald Ayoub <ronaldayoub@outlook.com>
2020-04-13 14:03:03 -0500

    bug

--- results/vast/cat_results.sh
+++ results/vast/cat_results.sh
@@ -5,6 +5,6 @@ ver=$2
 dir=${bm}_${ver}
 
 for id in $(find ${dir} -name '*.txt' -printf '%f\n'); do
-    awk -v ver=${ver} -v db_id=${id%.txt} -e 'BEGIN { FS = ","; OFS = ","; } { print ver, NR, db_id, tolower(substr($1,1,4)) substr($1,6,1), $3, $5 } NR==100 { exit }' ${dir}/${id}
+    awk -v ver=${ver} -v db_id=${id%.txt} -e 'BEGIN { FS = ","; OFS = ","; } { print ver, NR, db_id, tolower(substr($1,1,4)) substr($1,6,1), $3, $5 } ' ${dir}/${id}
 done;
 
