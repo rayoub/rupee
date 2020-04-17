db8637fa71bce15b9a6d5be248fef8b85e74ba07
Ronald Ayoub <ronaldayoub@outlook.com>
2020-04-17 09:37:40 -0500

    import vast

--- db/vast_result.sql
+++ db/vast_result.sql
@@ -2,6 +2,7 @@
 CREATE TABLE vast_result
 (
     version VARCHAR NOT NULL,
+    search_type VARCHAR NOT NULL,
     n INTEGER NOT NULL,
     db_id_1 VARCHAR NOT NULL,
     db_id_2 VARCHAR NOT NULL,
@@ -9,4 +10,4 @@ CREATE TABLE vast_result
     vast_rmsd NUMERIC NOT NULL
 ); 
 
-CREATE UNIQUE INDEX idx_vast_result_unique ON vast_result (version, n, db_id_1);
+CREATE UNIQUE INDEX idx_vast_result_unique ON vast_result (version, search_type, n, db_id_1);
--- db/x_vast_result.sql
+++ db/x_vast_result.sql
@@ -2,19 +2,21 @@
 TRUNCATE TABLE vast_result_staged;
 TRUNCATE TABLE vast_result;
 
-COPY vast_result_staged (version, n, db_id_1, db_id_2, vast_score, vast_rmsd) FROM '/home/ayoub/git/rupee/results/vast/vast_results.txt' WITH (DELIMITER ',');
+COPY vast_result_staged (version, n, db_id_1, db_id_2, vast_score, vast_rmsd, search_type) FROM '/home/ayoub/git/rupee/results/vast/vast_results_fl.txt' WITH (DELIMITER ',');
+COPY vast_result_staged (version, n, db_id_1, db_id_2, vast_score, vast_rmsd, search_type) FROM '/home/ayoub/git/rupee/results/vast/vast_results_rmsd.txt' WITH (DELIMITER ',');
 
-INSERT INTO vast_result (version, n, db_id_1, db_id_2, vast_score, vast_rmsd)
+INSERT INTO vast_result (version, n, db_id_1, db_id_2, vast_score, vast_rmsd, search_type)
 WITH results AS
 (
     SELECT
         version, 
+        search_type,
         n,
         db_id_1,
         db_id_2,
         vast_score,
         vast_rmsd,
-        ROW_NUMBER() OVER (PARTITION BY version, db_id_1, db_id_2 ORDER BY n) AS dupe_n
+        ROW_NUMBER() OVER (PARTITION BY version, search_type, db_id_1, db_id_2 ORDER BY n) AS dupe_n
     FROM
         vast_result_staged
 ),
@@ -22,7 +24,8 @@ filtered_results AS
 (
     SELECT
         version, 
-        ROW_NUMBER() OVER (PARTITION BY version, db_id_1 ORDER BY n) AS dedupe_n,
+        search_type,
+        ROW_NUMBER() OVER (PARTITION BY version, search_type, db_id_1 ORDER BY n) AS dedupe_n,
         db_id_1,
         db_id_2,
         vast_score,
@@ -38,12 +41,15 @@ SELECT
     db_id_1,
     db_id_2,
     vast_score,
-    vast_rmsd
+    vast_rmsd,
+    search_type
 FROM
     filtered_results
 WHERE
     dedupe_n <= 100
 ORDER BY
     version, 
+    search_type,
     db_id_1, 
     dedupe_n;
+
