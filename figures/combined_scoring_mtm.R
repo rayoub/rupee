fd64fbb070b7aa68f183ca806a9a5d7b05ad4c72
Ronald Ayoub <ronaldayoub@outlook.com>
2020-04-17 18:08:09 -0500

    plots

---
+++ figures/combined_scoring_mtm.R
@@ -0,0 +1,61 @@
+
+library(grid)
+library(gridExtra)
+
+source('scoring.R')
+
+grid_arrange_shared_legend <-
+  function(...,
+           ncol = 2, 
+           nrow = 1, 
+           position = c("bottom", "right")) {
+    
+    plots <- list(...)
+    position <- match.arg(position)
+    g <- ggplotGrob(plots[[1]] + theme(legend.position = position))$grobs
+    legend <- g[[which(sapply(g, function(x) x$name) == "guide-box")]]
+    lheight <- sum(legend$height)
+    lwidth <- sum(legend$width)
+    gl <- lapply(plots, function(x) x + theme(legend.position = "none"))
+    gl <- c(gl, ncol = ncol, nrow = nrow)
+    
+    combined <- switch(
+      position,
+      "bottom" = arrangeGrob(
+        do.call(arrangeGrob, gl),
+        legend,
+        ncol = 1,
+        heights = unit.c(unit(1, "npc") - lheight, lheight)
+      ),
+      "right" = arrangeGrob(
+        do.call(arrangeGrob, gl),
+        legend,
+        ncol = 2,
+        widths = unit.c(unit(1, "npc") - lwidth, lwidth)
+      )
+    )
+  }
+
+plot1 <- get_scoring_plot(
+        'vs. mTM-align',
+        'scoring_mtm_ci.txt', # _rmsd, _ci
+        c('RUPEE All-Aligned','RUPEE Top-Aligned','mTM'), 
+        c(1, 100),
+        c(1,10,20,30,40,50,60,70,80,90,100),
+        c(0.10, 0.65), # c(0, 5) or c(0.20, 0.80)
+        TRUE
+)
+plot2 <- get_scoring_plot(
+        'vs. mTM-align',
+        'scoring_mtm_rmsd.txt', # _rmsd, _ci
+        c('RUPEE All-Aligned','RUPEE Top-Aligned','mTM'), 
+        c(1, 10),
+        c(2,4,6,8,10),
+        c(0, 5), # c(0, 5) or c(0.20, 0.80)
+        FALSE
+)
+
+combined <- grid_arrange_shared_legend(plot1, plot2)
+
+ggsave('combined_scoring_mtm.eps', plot = combined, width = 7.5, height = 3.25, dpi = 300)
+
--- figures/scoring_test.R
+++ figures/scoring_test.R
@@ -28,13 +28,13 @@ source('scoring.R')
 #        c(0.3, 0.55), # c(0, 5), c(0.30, 0.55) or c(60, 75)
 #        FALSE
 #)
-plot <- get_scoring_plot(
+plot1 <- get_scoring_plot(
         'vs. VAST',
         'scoring_vast_fl.txt', # _ce, _fatcat, _fl
         c('RUPEE All-Aligned','RUPEE Top-Aligned','VAST'), 
         c(1, 100),
         c(1,10,20,30,40,50,60,70,80,90,100),
-        c(0.1, 0.6), # c(0, 5) or c(0.25, 0.80)
+        c(0.1, 0.6), # c(0, 5) or c(0.1, 0.6)
         TRUE
 )
 
