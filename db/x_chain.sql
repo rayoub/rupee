349925086fd46e7f74657ddd54c30ec0a5298191
Ronald Ayoub <ronaldayoub@outlook.com>
2020-04-26 10:37:04 -0500

    update chains

--- db/x_chain.sql
+++ db/x_chain.sql
@@ -1,7 +1,7 @@
 
 TRUNCATE chain;
 
-COPY chain (chain_id, pdb_id, chain_name, residue_count) FROM '/home/ayoub/git/rupee/data/chain/pdb_v01_01_2020.txt' WITH (DELIMITER ' ');
+COPY chain (chain_id, pdb_id, chain_name, residue_count) FROM '/home/ayoub/git/rupee/data/chain/pdb_v04_23_2020.txt' WITH (DELIMITER ' ');
 
 -- assign sort key
 UPDATE chain SET sort_key = chain_id;
