0f9ce72145846e25ae05be7def6394aae5213d83
Ronald Ayoub <ronaldayoub@outlook.com>
2020-08-27 10:30:08 -0500

    missing some variables

--- rupee-search/src/main/java/edu/umkc/rupee/search/lib/Constants.java
+++ rupee-search/src/main/java/edu/umkc/rupee/search/lib/Constants.java
@@ -5,23 +5,26 @@ public class Constants {
     public final static String APP_NAME = "RUPEE";
 
     public final static String DB_NAME = "rupee";
-   
-    public final static String DB_USER = "ayoub";
-    public final static String DB_PASSWORD = "ayoub";
+    
+    //public final static String DB_USER = "ayoub";
+    //public final static String DB_PASSWORD = "ayoub";
+
+    //public final static String SCOP_PATH = "/home/ayoub/git/rupee/data/scop/pdb/";
+    //public final static String CATH_PATH = "/home/ayoub/git/rupee/data/cath/pdb/";
+    //public final static String ECOD_PATH = "/home/ayoub/git/rupee/data/ecod/pdb/";
+    //public final static String CHAIN_PATH = "/home/ayoub/git/rupee/data/chain/pdb/";
+    //public final static String UPLOAD_PATH = "/home/ayoub/git/rupee/data/upload/";
+    
+    public final static String DB_USER = "ec2-user";
+    public final static String DB_PASSWORD = "ec2-user";
+
+    public final static String SCOP_PATH = "/home/ec2-user/data/scop/pdb/";
+    public final static String CATH_PATH = "/home/ec2-user/data/cath/pdb/";
+    public final static String ECOD_PATH = "/home/ec2-user/data/ecod/pdb/";
+    public final static String CHAIN_PATH = "/home/ec2-user/data/chain/pdb/";
+    public final static String UPLOAD_PATH = "/home/ec2-user/data/upload/";
     
     public final static String DIR_PATH = "/home/ayoub/testdir/";
-
-    public final static String SCOP_PATH = "/home/ayoub/git/rupee/data/scop/pdb/";
-    public final static String CATH_PATH = "/home/ayoub/git/rupee/data/cath/pdb/";
-    public final static String ECOD_PATH = "/home/ayoub/git/rupee/data/ecod/pdb/";
-    public final static String CHAIN_PATH = "/home/ayoub/git/rupee/data/chain/pdb/";
-    public final static String UPLOAD_PATH = "/home/ayoub/git/rupee/data/upload/";
-
-    //public final static String SCOP_PATH = "/home/ec2-user/data/scop/pdb/";
-    //public final static String CATH_PATH = "/home/ec2-user/data/cath/pdb/";
-    //public final static String ECOD_PATH = "/home/ec2-user/data/ecod/pdb/";
-    //public final static String CHAIN_PATH = "/home/ec2-user/data/chain/pdb/";
-    //public final static String UPLOAD_PATH = "/home/ec2-user/data/upload/";
     
     public final static int MIN_GRAM_COUNT = 10;
     public final static int MIN_HASH_COUNT = 99;
