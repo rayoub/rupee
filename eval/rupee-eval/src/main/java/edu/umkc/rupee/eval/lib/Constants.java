bea0b8ce952e6ba9910ff6e098da78069afcf7c5
Ronald Ayoub <ronaldayoub@outlook.com>
2020-09-08 09:40:57 -0500

    name

--- eval/rupee-eval/src/main/java/edu/umkc/rupee/eval/lib/Constants.java
+++ eval/rupee-eval/src/main/java/edu/umkc/rupee/eval/lib/Constants.java
@@ -15,9 +15,9 @@ public class Constants {
     public final static String CATHEDRAL_PATH = "/home/ayoub/git/rupee/results/cathedral/temp/";
     public final static String VAST_PATH = "/home/ayoub/git/rupee/results/vast/";
     public final static String VAST_PATH_RMSD 
-        = VAST_PATH + "casp_d250_casp_chain_v01_01_2020_rmsd/";
+        = VAST_PATH + "casp_d250_casp_chain_v08_28_2020_rmsd/";
     public final static String VAST_PATH_VAST_SCORE 
-        = VAST_PATH + "casp_d250_casp_chain_v01_01_2020_full_length/";
+        = VAST_PATH + "casp_d250_casp_chain_v08_28_2020_full_length/";
     
     public final static String CASP_PATH = "/home/ayoub/git/rupee/data/casp/eu_preds/";
 }
