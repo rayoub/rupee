72602b5d8ddab639d77ac72860d93685dad293ed
Ronald Ayoub <ronaldayoub@outlook.com>
2020-09-08 16:08:08 -0500

    eval create all script

---
+++ eval/db/y_create_all.sql
@@ -0,0 +1,24 @@
+
+-- tables
+alignment_scores.sql
+benchmark.sql
+cathedral_result.sql
+mtm_result.sql
+response_time.sql
+rupee_result.sql
+ssap_result.sql
+ssm_result.sql
+ssm_result_staged.sql
+vast_request.sql
+vast_result.sql
+vast_result_staged.sql
+
+-- functions
+get_alignment_scores.sql
+get_benchmark_split.sql
+get_cathedral_results.sql
+get_mtm_results.sql
+get_rupee_results.sql
+get_ssm_results.sql
+get_vast_results.sql
+insert_alignment_scores.sql
