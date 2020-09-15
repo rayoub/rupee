efdef62eb36d1e5e1c50e1bcf7ecbac092242c9c
Ronald Ayoub <ronaldayoub@outlook.com>
2020-09-15 07:40:06 -0500

    fast

--- eval/results/rupee/casp.sh
+++ eval/results/rupee/casp.sh
@@ -6,5 +6,5 @@ bm=$1
 ver=$2
 id=$3
 
-java -jar ../../rupee-eval/target/rupee-eval-0.0.1-SNAPSHOT-jar-with-dependencies.jar -u CHAIN,../../../data/casp/eu_preds/${id}.pdb,FALSE,FALSE,FALSE,FALSE,FALSE,FALSE,ALL_ALIGNED,CONTAINED_IN > ./${bm}_${ver}/${id}.txt
+java -jar ../../rupee-eval/target/rupee-eval-0.0.1-SNAPSHOT-jar-with-dependencies.jar -u CHAIN,../../../data/casp/eu_preds/${id}.pdb,FALSE,FALSE,FALSE,FALSE,FALSE,FALSE,FAST,CONTAINED_IN > ./${bm}_${ver}/${id}.txt
 
