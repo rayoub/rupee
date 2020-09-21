920520118e2abfd2feec46cb5728e2e74767bb84
Ronald Ayoub <ronaldayoub@outlook.com>
2020-09-21 12:04:34 -0500

    update

--- eval/results/rupee/rupee.sh
+++ eval/results/rupee/rupee.sh
@@ -6,4 +6,5 @@ bm=$1
 ver=$2
 id=$3
 
-java -jar ../../rupee-mgr/target/rupee-mgr-0.0.1-SNAPSHOT-jar-with-dependencies.jar -s DB_ID,SCOP,${id},100,FALSE,FALSE,FALSE,FALSE,FALSE,FALSE,TOP_ALIGNED,TM_SCORE > ./${bm}_${ver}/${id}.txt
+java -jar ../../rupee-eval/target/rupee-eval-0.0.1-SNAPSHOT-jar-with-dependencies.jar -s SCOP,${id},FALSE,FALSE,FALSE,FALSE,FALSE,FALSE,ALL_ALIGNED,FULL_LENGTH > ./${bm}_${ver}/${id}.txt
+
