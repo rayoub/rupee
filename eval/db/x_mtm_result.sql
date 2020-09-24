10028adb0d8654d518cd76e8b6514f6d816481c7
Ronald Ayoub <ronaldayoub@outlook.com>
2020-09-24 09:44:00 -0500

    import results

--- eval/db/x_mtm_result.sql
+++ eval/db/x_mtm_result.sql
@@ -1,36 +1,8 @@
 
--- important notes:
--- mTM does a better job of parsing therefore they will have some pdb entries that are missing from RUPEE.
--- To be fair to mTM we need to take the matches they found and enter them into the alignment_scores table. 
--- mTM should be awarded for good parsing rather than inadvertantly filtering out unmatched data. 
--- When correctly normalized, I have not found a case where their TM-align score does not match RUPEE.
-
 TRUNCATE TABLE mtm_result;
 
 COPY mtm_result (version, n, db_id_1, db_id_2, mtm_rmsd, mtm_tm_score) FROM '/home/ayoub/git/rupee/eval/results/mtm/mtm_results.txt' WITH (DELIMITER ',');
 
--- don't do this any more we can fix this in the get functions with a left join 
-INSERT INTO alignment_scores (version, db_id_1, db_id_2, tm_q_tm_score, tm_rmsd)
-WITH filtered AS
-(
-    SELECT
-        r.*
-    FROM
-        mtm_result r
-        LEFT JOIN alignment_scores s
-            ON r.version = s.version
-            AND r.db_id_1 = s.db_id_1
-            AND r.db_id_2 = s.db_id_2
-    WHERE
-        s.version IS NULL
-        AND r.n <= 100
-)
-SELECT
-    version,
-    db_id_1,
-    db_id_2,
-    mtm_tm_score,
-    mtm_rmsd
-FROM
-    filtered;
+DELETE FROM mtm_result WHERE n > 100;
+
 
