8dc4429d9330283b04a699dba267bcac396bc1df
Ronald Ayoub <ronaldayoub@outlook.com>
2020-10-06 09:03:37 -0500

    useful

---
+++ eval/figures/copy_figs.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+dest=../../../notes/prot2020
+
+cp combined_scoring_mtm.eps ${dest}/Fig5.eps
+cp combined_scoring_ssm.eps ${dest}/Fig6.eps
+cp combined_scoring_cathedral.eps ${dest}/Fig7.eps
+cp combined_scoring_vast.eps ${dest}/Fig8.eps
+cp combined_scoring_rupee.eps ${dest}/Fig9.eps
+cp combined_response.eps ${dest}/Fig10.eps
+cp combined_response_rupee.eps ${dest}/Fig11.eps
